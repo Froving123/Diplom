1f9645a180378a69e1aed586b743430da8cfcfed
Froving123 <chernyh.sasha26@gmail.com>
2024-02-19 19:22:14 +0500

    14

---
+++ restaurant/app/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import styles from "../Header/Header.module.css";
+
+const Navbar = () => {
+    return(
+        <nav>
+            <div>
+                Меню
+            </div>
+            <div>
+                Доставка
+            </div>
+            <div>
+                Корзина
+            </div>
+            <div>
+                Профиль
+            </div>
+        </nav>
+    )
+}
