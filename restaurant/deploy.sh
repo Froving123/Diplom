1257f6e37e3bb4bbbe96f1c136005fb70b380f28
Froving123 <chernyh.sasha26@gmail.com>
2024-11-10 15:54:28 +0500

    no firebase

--- restaurant/deploy.sh
+++
@@ -1,8 +0,0 @@
-# Установка политики выполнения
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-
-# Включение экспериментов Firebase
-firebase experiments:enable webframeworks
-
-# Развертывание Firebase
-firebase deploy
