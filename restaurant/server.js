910b700158ee4ebd09485693d0cb2dbfdf565982
Froving123 <chernyh.sasha26@gmail.com>
2024-11-13 18:40:34 +0500

    server- res_req

--- restaurant/server.js
+++ restaurant/server.js
@@ -1,10 +1,104 @@
-const express = require('express');
+const express = require("express");
 const server = express();
 
-try {
-  server.listen(5000, () => {
-    console.log('Server started on port 5000');
-  });
-} catch (err) {
-  console.error('Error starting server:', err);
-}
+server.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
+
+server.get('/', (req, res) => {
+    res.send('Server working')
+})
+
+//authForm
+//import { auth } from "@/app/firebase";
+//import { signInWithEmailAndPassword } from "firebase/auth";
+//import { createUserWithEmailAndPassword } from "firebase/auth";
+
+/*
+DeliveryForm
+import {
+  ref,
+  push,
+  set,
+  query,
+  orderByChild,
+  equalTo,
+  onValue,
+  update,
+} from "firebase/database";
+import { db, auth } from "../../firebase";
+
+
+Delivery_menu
+import { ref, push, set } from "firebase/database";
+import { db, auth } from "../../firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+
+Delivery_user
+import {
+  ref,
+  get,
+  set,
+  query,
+  orderByChild,
+  equalTo,
+  onValue,
+} from "firebase/database";
+import { db, auth } from "@/app/firebase";
+
+
+Forma_feedback
+import { onAuthStateChanged } from "firebase/auth";
+import { ref, push, set } from "firebase/database";
+import { auth, db } from "@/app/firebase";
+
+
+Header.
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/app/firebase";
+
+
+ReservForm
+import { ref, push, set } from "firebase/database";
+import { db, auth } from "../../firebase";
+
+
+Reservations
+import { ref, get, query, orderByChild, equalTo } from "firebase/database";
+import { db, auth } from "@/app/firebase";
+
+
+Shopping_cart
+import {
+  ref,
+  onValue,
+  query,
+  orderByChild,
+  equalTo,
+  remove,
+  update,
+} from "firebase/database";
+import { db, auth } from "@/app/firebase";\
+
+
+Shopping_cart_button.
+import {
+  ref,
+  set,
+  query,
+  orderByChild,
+  equalTo,
+  onValue,
+} from "firebase/database";
+ import { db, auth } from "@/app/firebase";
+
+
+ Welcome
+ import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/app/firebase";
+
+
+\AuthDetails
+import { auth } from "@/app/firebase";
+import { onAuthStateChanged, signOut } from "firebase/auth";
