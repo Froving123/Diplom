0a98802ec8e6d01d8edeb71bd80287f3be90f64a
Froving123 <chernyh.sasha26@gmail.com>
2024-11-18 20:46:31 +0500

    registr no work

--- restaurant/controllers/userController.js
+++ restaurant/controllers/userController.js
@@ -1,15 +1,59 @@
+const bcrypt = require("bcrypt");
+
+/*server.get('/users', (req, res) => {
+    conn.query("SELECT * FROM Пользователь", (err, result, fields) => {
+      if (err) {
+        return res.status(500).json({ error: err });
+      }
+      res.json(result);
+    });
+  });*/
+
 class UserController {
-    async registration(req, res) {
-
+  async registration(req, res) {
+    const { email, password, copyPassword, number, last, name, fat } = req.body;
+    if (
+      !email ||
+      !password ||
+      !copyPassword ||
+      !last ||
+      !name ||
+      !fat ||
+      !number
+    ) {
+      setError("Пожалуйста, заполните все поля");
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    } else {
+      if (copyPassword !== password) {
+        setError("пароли не совпадают");
+        setTimeout(() => {
+          setError("");
+        }, 5000);
+      }
+      const candidate = await dbData.findOne({ where: { email, number } });
+      if (candidate) {
+        setError("Такой пользователь уже существует");
+        setTimeout(() => {
+          setError("");
+        }, 5000);
+      }
+      const hashPassword = await bcrypt.hash(password, 5);
+      const user = await dbData.create({
+        email,
+        password: hashPassword,
+        number,
+        last,
+        name,
+        fat,
+      });
     }
+  }
 
-    async login(req, res) {
+  async login(req, res) {}
 
-    }
-
-    async check(req, res) {
-
-    }
+  async check(req, res) {}
 }
 
-module.exports = new UserController()
+module.exports = new UserController();
--- restaurant/server.js
+++ restaurant/server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
-const bodyParser = require('body-parser');
-const router = require('./routes/index')
+const bodyParser = require("body-parser");
+const router = require("./routes/index");
 
 const server = express();
 const PORT = 5000;
 server.use(cors());
 server.use(bodyParser.json());
-server.use('/api', router)
+server.use("/api", router);
 
 const conn = mysql.createConnection({
   host: "MySQL-8.0",
