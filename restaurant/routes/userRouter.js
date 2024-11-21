ece68c5d9dab5adfa5f654ec1cf067ab5b7e6add
Froving123 <chernyh.sasha26@gmail.com>
2024-11-21 18:14:12 +0500

    rout work

--- restaurant/routes/userRouter.js
+++ restaurant/routes/userRouter.js
@@ -2,9 +2,7 @@ const Router = require("express");
 const router = new Router();
 const userController = require("../controllers/userController");
 
-router.post("/registration", (req, res) => {
-  userController.registration(req, res);
-});
+router.post("/registration", userController.registration);
 router.post("/login", userController.login);
 router.get("/auth", userController.check);
 
