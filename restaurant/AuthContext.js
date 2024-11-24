1d4b847ace9a40745f04a081834be37be8efca60
Froving123 <chernyh.sasha26@gmail.com>
2024-11-24 20:32:48 +0500

    AuthContext

---
+++ restaurant/AuthContext.js
@@ -0,0 +1,32 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [authUser, setAuthUser] = useState(null);
+
+  const login = (token) => {
+    localStorage.setItem("authToken", token);
+    setAuthUser({ token });
+  };
+
+  const logout = () => {
+    localStorage.removeItem("authToken");
+    setAuthUser(null);
+  };
+
+  const register = (token) => {
+    localStorage.setItem("authToken", token);
+    setAuthUser({ token });
+  };
+
+  return (
+    <AuthContext.Provider value={{ authUser, login, logout, register }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
