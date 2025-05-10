b8bbdc5947be4174b0c96b246c912a821b0d4f7b
Froving123 <chernyh.sasha26@gmail.com>
2025-05-10 19:02:24 +0500

    1

--- restaurant/app/Profile/page.js
+++ restaurant/app/Profile/page.js
@@ -3,7 +3,7 @@ import { AuthDetails } from "../components/Auth/AuthDetails";
 export default function Home() {
   return (
     <main>
-      <AuthDetails></AuthDetails>
+      <AuthDetails />
     </main>
   );
 }
