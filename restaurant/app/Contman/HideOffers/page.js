2377bb65d5e8c10cd327feeeed47d998c9ac2cef
Froving123 <chernyh.sasha26@gmail.com>
2025-05-19 19:26:33 +0500

    2

--- restaurant/app/Contman/HideOffers/page.js
+++ restaurant/app/Contman/HideOffers/page.js
@@ -1,8 +1,9 @@
 import { Hide_Offers } from "../../components/Hide_Offers/Hide_Offers";
+import Styles from "./HideOffers.module.css";
 
 export default function Home() {
   return (
-    <main>
+    <main className={Styles.back}>
       <Hide_Offers />
     </main>
   );
